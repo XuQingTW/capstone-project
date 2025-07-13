ea73f56f097bc376fd8ce80b9cf93f39d4678001
A445121 <g51100526@gmail.com>
2025-07-13 22:42:21 +0800

    fix(database.py):符合flake8要求

--- src/database.py
+++ src/database.py
@@ -148,7 +148,7 @@ class Database:
                     [event_time] datetime2(2) NOT NULL,
                     [detected_anomaly_type] NVARCHAR(MAX) NOT NULL,
                     [downtime_min] INT NULL,
-                    [downtime_sec] INT NULL,    
+                    [downtime_sec] INT NULL,
                     [resolved_time] datetime2(2) NULL,
                     [notes] NVARCHAR(MAX) NULL
                 """
