4f33cbf688edcb93543b37f50f95c87f52291914
A445121 <g51100526@gmail.com>
2025-08-15 18:02:44 +0800

    feat(database.py):新增alert_type做為條件讓警報定位更加精確(須測試)

--- src/database.py
+++ src/database.py
@@ -544,26 +544,26 @@ class Database:
             if conn:
                 conn.close()
 
-    def get_alert_info(self, error_id: int):
-        """用 error_id 取得單筆警報的資訊"""
-        sql = "SELECT equipment_id, alert_type FROM alert_history WHERE error_id = ?;"
+    def get_alert_info(self, error_id: int, alert_type: str):
+        """用 error_id 跟 alert_type 取得單筆警報的資訊"""
+        sql = "SELECT equipment_id, alert_type FROM alert_history WHERE error_id = ? AND alert_type =  ?;"
         try:
             with self._get_connection() as conn:
                 cursor = conn.cursor()
-                cursor.execute(sql, error_id)  # 執行SQL查詢 並將error_id作為參數傳入
+                cursor.execute(sql, error_id, alert_type)  # 執行SQL查詢 並將 error_id 跟 alert_type 作為參數傳入
                 row = cursor.fetchone()  # 從查詢結果中取出唯一一筆資料
                 if row:  # 檢查是否有成功取回資料
-                    return {"equipment_id": row[0], "alert_type": row[1]}
+                    return {"equipment_id": row[0]}
                 return None
         except pyodbc.Error as e:
-            logger.error(f"查詢警報資訊 (error_id: {error_id}) 失敗: {e}")
+            logger.error(f"查詢警報資訊 (error_id: {error_id}), alert_type: {alert_type}) 失敗: {e}")
             return None
 
     def resolve_alert_history(self, log_data: dict):
         """
         將指定的警報紀錄更新為已解決狀態
         """
-        # 更新指定 alert_history 欄位內容，依照error_id作為條件
+        # 更新指定 alert_history 欄位內容，依照 error_id 跟 alert_type 作為條件
         sql = """
         UPDATE alert_history
            SET is_resolved = 1,
@@ -571,7 +571,7 @@ class Database:
                resolved_by = ?,
                resolution_notes = ?
         OUTPUT inserted.resolved_time
-         WHERE error_id = ? AND (is_resolved = 0);
+         WHERE error_id = ? AND alert_type = ? AND (is_resolved = 0);
         """
         conn = None
         try:
@@ -584,7 +584,8 @@ class Database:
             cursor.execute(sql,
                            log_data["resolved_by"],
                            notes,
-                           log_data["error_id"]
+                           log_data["error_id"],
+                           log_data["alert_type"]
                            )
 
             newly_resolved_time = cursor.fetchone()  # 取得更新後 OUTPUT 的時間
@@ -595,22 +596,23 @@ class Database:
                 return newly_resolved_time[0]
             else:
                 # 檢查這筆警報是否是已解決
-                check_sql = "SELECT resolved_time FROM alert_history WHERE error_id = ? AND is_resolved = 1;"
-                cursor.execute(check_sql, log_data['error_id'])
+                check_sql = "SELECT resolved_time FROM alert_history WHERE error_id = ? AND alert_type = ? AND is_resolved = 1;"
+                cursor.execute(check_sql, log_data['error_id'], log_data['alert_type'])
                 already_resolved_time = cursor.fetchone()
 
                 if already_resolved_time:
                     # 警報先前已是解決狀態
-                    logger.info(f"嘗試解決的 error_id: {log_data['error_id']} 先前已被解決。")
+                    logger.info(f"嘗試解決的 error_id: {log_data['error_id']} / alert_type: {log_data['alert_type']} 先前已被解決。")
                     return (already_resolved_time[0], "already_resolved")
                 else:
                     # 資料庫不存在這筆 error_id
-                    logger.warning(f"嘗試更新警報，但找不到對應的 error_id: {log_data['error_id']}。")
+                    logger.warning(f"嘗試更新警報，但找不到對應的 error_id: {log_data['error_id']} 和 alert_type: {log_data['alert_type']}。")
                     return None
 
         except pyodbc.Error as ex:
             error_id_val = log_data.get('error_id', 'N/A')   # 取得 error_id 或預設N/A'
-            logger.error(f"更新警報 (error_id: {error_id_val}) 時發生資料庫錯誤: {ex}")
+            alert_type_val = log_data.get('alert_type', 'N/A') # 取得 alert_type 或預設N/A'
+            logger.error(f"更新警報 (error_id: {error_id_val}), alert_type: {alert_type_val}) 時發生資料庫錯誤: {ex}")
             if conn:
                 conn.rollback()
                 logger.warning("交易已回滾。")
